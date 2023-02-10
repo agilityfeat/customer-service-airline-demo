399b4cba4e8f601fa29c7227182c8b44f3d4e09f
Fahad-Mahmood <fahad492@outlook.com>
2023-02-10 12:48:49 +0500

    feat: agent home, summary screen ui updated

--- src/components/agentSummary/index.tsx
+++ src/components/agentSummary/index.tsx
@@ -1,34 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { format } from 'date-fns';
 import { PrimaryButton } from '@/components/buttons/primary';
+import { Header } from '../header';
+import { Main } from '../main';
+import { QuestionInsights } from '../questionInsights';
+import { CallSentiments } from '../call-sentiments';
+
 import styles from './index.module.scss';
 
 
-const ACCESS_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjYwNTQzODg5Mzk4ODI0OTYiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiVnl4TjhucXNjcHVodXpOVGhtZ2ZjT0NvS1Y4NlBYR2hAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjc1MjY1NTk4LCJleHAiOjE2NzUzNTE5OTgsImF6cCI6IlZ5eE44bnFzY3B1aHV6TlRobWdmY09Db0tWODZQWEdoIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.1oq65ohJ4PlkmOQrOdJ0gjEgC0v3NyHCLdxtcu0uWk2TAYIxGuEXTg7FJzDRYMsGfs-_wHVg32A53YGUgjTLFnKOz-BXetDPa1fPB-3UZ8GD85sLifUzz1GVSMfKO0SdR8frXp-r5-GvAHV--WdpzcAMBrosbjYDS-N7C5XfCIeYwxRzEeO08QTk1ssOmPLKve_iSaY9NRnjOZTiV_oe7diYxT1QITSkH_rU-5f-yOS_0x8S9XK2H1mbyY6llqYIXfjkt_HptIaTcq8auVShZtJomUava8lY4DaU88MFk-Gks7wKY3rqdNKD_EYHw5timkwcDrin6ci-CnBG3Gw6DA';
+
+const ACCESS_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjYwNTQzODg5Mzk4ODI0OTYiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiVnl4TjhucXNjcHVodXpOVGhtZ2ZjT0NvS1Y4NlBYR2hAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjc1OTUyNTAzLCJleHAiOjE2NzYwMzg5MDMsImF6cCI6IlZ5eE44bnFzY3B1aHV6TlRobWdmY09Db0tWODZQWEdoIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.Tvo0273gpMCLW36KoTwUJCWeuWiZqe1WyokLfFKKk9n5-q2aahDyNyHBIdg8MqwRqG--hKqVS9JW3QIKDN-zWdtVkP3FgCP7vyl7n-wNCN4xcxFwPSHgFDUnJaAkMwKQpOeL7Ds5o1uFES5-koyrJj4pDJ0f99zsM5O4ynOatA8GKDnuV0Av9A5ZuzJ8hx0iSkn5I3WMnmSrdgjXeFnVDDtYOmKiBw115u99ZhARbk7mDqlAW1YwP-Kx0vYBOXciwlA0k1sOAJLIkuId41Hs9oMy9bjnMcXF1YbaWco4ZqRk9bDwkhGv8ETY8dulyiEWO5jWqaYo62bO5zgVpzFfwg';
 const BASE_URI = 'https://api.symbl.ai/v1';
 
 
-const TITLE = 'Call Insights';
-const MESSAGE_PLACEHOLDER = 'Type Message For Customer'
+const TITLE1 = 'Audio Tracks';
+const TITLE2 = 'Send text message to customer';
+const MESSAGE_PLACEHOLDER = 'Type Message For Customer';
+const AGENT_USER_ID = 'fahad.mahmoood@agilityfeat.com';
 
 const AgentSummary = function Home() {
   const [message, setMessage] = useState('');
   const [summary, setSummary] = useState('');
+  const [questions, setCustomerQuestionInsights] = useState([]);
+  const [symblMessages, setSymblMessages] = useState([]);
   const router = useRouter();
   const {conversationId} = router.query;
 
 
+  const fetchQuestionInsights = async () => {
+      const {data} = await axios.get(`${BASE_URI}/conversations/${conversationId}/questions`, {headers: {'Authorization': `Bearer ${ACCESS_TOKEN}`}});
+      if(data?.questions) {
+        setCustomerQuestionInsights(data?.questions);
+      }
+  }
 
-  const fetchSummary = async (conversationId) => {
+
+  const getMessageAuthor = (userId: string) => {
+    if(userId === AGENT_USER_ID) return 'Agent';
+    return 'Customer';
+  }
+
+  const fetchMessageResponses= async () => {
+    const {data} = await axios.get(`${BASE_URI}/conversations/${conversationId}/messages?sentiment=true`, {headers: {'Authorization': `Bearer ${ACCESS_TOKEN}`}});
+
+    if(data?.messages) {
+      const newMessageResponses= data.messages.map((messageResponse: any) =>{
+        const messageObject =  {id: messageResponse?.id, transcript: messageResponse?.text, sentiment: messageResponse?.sentiment?.suggested, from: getMessageAuthor(messageResponse.from.id), time: format(new Date(messageResponse?.endTime), 'h:mm aaa')};
+        return messageObject;
+        });
+        setSymblMessages(newMessageResponses);
+    }
+}
+  const fetchSummary = async () => {
     const {data} = await axios.get(`${BASE_URI}/conversations/${conversationId}/summary`, {headers: {'Authorization': `Bearer ${ACCESS_TOKEN}`}});
     setSummary(data.summary[0].text);
   };
 
   useEffect(() => {
     if(conversationId) {
-        fetchSummary(conversationId)
+        fetchSummary()
+        fetchQuestionInsights();
+        fetchMessageResponses();
     }
     
   }, [conversationId]);
@@ -53,21 +90,34 @@ const AgentSummary = function Home() {
   }
  
   return (
-    <div className={styles.main}>
+    <>
+    <Header/>
+    <Main>
     <div className={styles.container}>
-        <h1 className={styles.heading}>{TITLE}</h1>
-    </div>
-
-    <div className={styles.summaryContainer}>
-    <h2 className={styles.heading}>Conversation Summary</h2>
-    <p>{summary}</p>
-    </div>
-    <div className={styles.messageContainer}>
-    <textarea onChange={onMessageChange} className={styles.textarea} placeholder={MESSAGE_PLACEHOLDER} rows={20} name="comment[text]" id="comment_text" cols={40}  aria-autocomplete="list" aria-haspopup="true" />
+      <div className={styles.firstSection}>
+       <h3 className={styles.heading}>{TITLE1}</h3>
+       <div>
+        <p className={styles.graphText}>Agent</p>
+        <Image height={60} width={600} alt='audio track' src='/assets/track1.svg' />
+       </div>
+       <div>
+        <p className={styles.graphText}>Customer</p>
+        <Image height={60} width={600} alt='audio track' src='/assets/track1.svg' />
+       </div>
+       <h3 className={styles.heading}>{TITLE2}</h3>
+       <div className={styles.messageContainer}>
+       <textarea onChange={onMessageChange} className={styles.textarea} placeholder={MESSAGE_PLACEHOLDER} rows={20} name="comment[text]" id="comment_text" cols={40}  aria-autocomplete="list" aria-haspopup="true" />
     <PrimaryButton onClick={onSendSMS}>Send SMS</PrimaryButton>
+      </div>
+      </div>
+      <div className={styles.secondSection}>
+        <QuestionInsights customerQuestionInsights={questions} />
+        <CallSentiments symblMessages={symblMessages} />
+      </div>
     </div>
-    </div>
+    </Main>
 
+</>
   )
 };
 
