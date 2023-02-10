8288a58671139e3308b105c8b8af40e1e054faa2
Fahad-Mahmood <fahad492@outlook.com>
2023-02-10 12:05:09 +0500

    feat: nunitoSans font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
+import { Nunito_Sans } from '@next/font/google'
 import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin', 'cyrillic', 'latin'],
+  weight: ['200', '300', '400', '600', '700', '800', '900']
+})
 
 const MyApp = function app ({ Component, pageProps }: AppProps) {
 
   return (
-  <>
+<main className={nunitoSans.className}>
   <ToastContainer />
     <Component {...pageProps} />
-  </>
+  </main>
   );
 };
 export default MyApp;
